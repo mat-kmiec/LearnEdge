1d10cafb6c6de3b9f2f521076692eaadb972a4c0
server133602 <server133602@server133602.nazwa.pl>
2025-10-15 21:20:14 +0200

    Initial commit - LearnEdge project setup

---
+++ src/main/java/pl/eduadapt/EduAdaptApplication.java
@@ -0,0 +1,13 @@
+package pl.eduadapt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduAdaptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduAdaptApplication.class, args);
+    }
+
+}
