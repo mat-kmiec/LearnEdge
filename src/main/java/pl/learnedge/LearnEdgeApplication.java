5300e5f00af18e1860311184c96d9ecdb11e8757
Mateusz Kmiec <mat-kmiec@users.noreply.github.com>
2025-10-16 02:29:55 +0200

    change main class spring start

--- src/main/java/pl/learnedge/LearnEdgeApplication.java
+++ src/main/java/pl/learnedge/LearnEdgeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EduAdaptApplication {
+public class LearnEdgeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EduAdaptApplication.class, args);
+        SpringApplication.run(LearnEdgeApplication.class, args);
     }
 
 }
